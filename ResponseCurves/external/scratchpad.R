978c6e58376d08f98802325030178dd4b6e17ee3
Talbert <MTALBERT@USGS.GOV>
2015-11-16 10:51:22 -0700

    trying to incorperate biomod

--- ResponseCurves/external/scratchpad.R
+++ ResponseCurves/external/scratchpad.R
@@ -1,23 +1,60 @@
-ggplot(data = distribs, aes(x = values, group = type)) +
-  geom_density(aes(fill = type, color = type), alpha = 0.5)
+library(biomod2)
+DataSpecies <- read.csv(system.file("external/species/mammals_table.csv",
+                                    package="biomod2"), row.names = 1)
+head(DataSpecies)
 
-m <- ggplot(movies, aes(x = rating))
-m + geom_density()
-d<-data.frame(predicted=unlist(predictedVals),Response=rep(resp,times=length(fitLst)),
-                Model=rep(names(fitLst),each=length(resp)))
-d$Model<-as.factor(d$Model)
-d$Response<-as.factor(d$Response)
-m<-ggplot(d,aes(x=predicted,colour=Response,fill=Response))+geom_density(alpha=0.3)+facet_wrap(~ Model)+
-  scale_fill_manual(values=c("blue","red"))+
-  scale_colour_manual(values=c("blue","red"))
-m
+# the name of studied species
+myRespName <- 'GuloGulo'
 
-ggplot(data = distribs, aes(x = values, group = type)) +
-  geom_density(aes(fill = type, color = type), alpha = 0.5)
+# the presence/absences data for our species 
+myResp <- as.numeric(DataSpecies[,myRespName])
 
-ggplot(Dat,aes(x=Temp,colour=Emissions,fill=Emissions)) +
-  scale_fill_manual(values=EmissionsCol)+
-  scale_colour_manual(values=EmissionsCol)+
-  geom_density(alpha=0.3) +
-  theme_bw() +
-  theme0(plot.margin = unit(c(1,0,0,2.2),"lines"))
+# the XY coordinates of species data
+myRespXY <- DataSpecies[,c("X_WGS84","Y_WGS84")]
+
+
+# Environmental variables extracted from BIOCLIM (bio_3, bio_4, bio_7, bio_11 & bio_12)
+myExpl = stack( system.file( "external/bioclim/current/bio3.grd", 
+                             package="biomod2"),
+                system.file( "external/bioclim/current/bio4.grd", 
+                             package="biomod2"), 
+                system.file( "external/bioclim/current/bio7.grd", 
+                             package="biomod2"),  
+                system.file( "external/bioclim/current/bio11.grd", 
+                             package="biomod2"), 
+                system.file( "external/bioclim/current/bio12.grd", 
+                             package="biomod2"))
+
+# 1. Formatting Data
+myBiomodData <- BIOMOD_FormatingData(resp.var = myResp,
+                                     expl.var = myExpl,
+                                     resp.xy = myRespXY,
+                                     resp.name = myRespName)
+SpDataFrame<-cbind(myBiomodData@coord,myBiomodData@data.species,myBiomodData@data.env.var)
+correlationViewer(data=SpDataFrame)
+# 2. Defining Models Options using default options.
+myBiomodOption <- BIOMOD_ModelingOptions()
+
+# 3. Doing Modelisation
+
+myBiomodModelOut <- BIOMOD_Modeling( myBiomodData, 
+                                     models = c('GLM','GBM','GAM','CTA','ANN',
+                                                'FDA','MARS','RF'), 
+                                     models.options = myBiomodOption, 
+                                     NbRunEval=1, 
+                                     DataSplit=70, 
+                                     models.eval.meth = c('TSS','ROC'),
+                                     SaveObj = TRUE,
+                                     do.full.models = FALSE)
+
+# 4. Loading some models built
+ModelNames<-substr(myBiomodModelOut@models.computed,nchar(myBiomodModelOut@models.computed)-2,
+                   nchar(myBiomodModelOut@models.computed))
+ModelNames<-unique(gsub("_","",ModelNames))
+ModelNames<-gsub("ARS","MARS",ModelNames)
+myLoadedModels <- BIOMOD_LoadModels(myBiomodModelOut, models=ModelNames)
+
+ModelNames<-myBiomodModelOut@models.computed
+FitLst<-lapply(ModelNames,get_formal_model)
+eval("GuloGulo_AllData_RUN1_GBM")
+get_formal_model(GuloGulo_AllData_RUN1_GBM)
