4363d03527094aa8d3322bf52b33860baeeedccc
Talbert <MTALBERT@USGS.GOV>
2015-12-30 12:23:21 -0700

    initial working ggplot2 pairs explore I think I could build this into a shiny app for initial data inspection

--- ggplot2pairsTry.R
+++ ggplot2pairsTry.R
@@ -3,97 +3,56 @@ dat<-myBiomodData@data.env.var
 dat$resp<-as.factor(myBiomodData@data.species)
 par(mfrow=c(ncol(dat),ncol(dat)),mar=c(0,0,0,0),oma=c(0,0,0,0))
 hist(dat[,1],main=names(dat)[1])
-
+d<-data.frame(x=c(0,1),y=c(0,1))
 grid.newpage()
-pushViewport(viewport(layout=grid.layout(ncol(dat),ncol(dat))))
+pushViewport(viewport(layout=grid.layout((ncol(dat)-1),(ncol(dat)-1))))
 vplayout<- function(x,y)
   viewport(layout.pos.row=x, layout.pos.col=y)
 for(i in 1:(ncol(dat)-1)){
 print(qplot(dat[,i],
             geom="histogram",
-            main = names(dat[,i]), 
-            xlab = "Age",  
-            fill=I("blue")),vp=vplayout(i,i))
+            xlab = names(dat)[i], 
+            ylab = "",  
+            fill=I("blue"),
+            colour=I("black"))+scale_y_discrete(breaks=NULL)+scale_x_discrete(breaks=NULL)+
+            theme(plot.margin=unit(c(0,0,0,0),"mm")),vp=vplayout(i,i))
 #pairs plot below the diagonal
   if(i>1){
-  for(j in 1:(i-1)){
-  print(
-  ggplot(dat, 
-         aes_q(x = as.name(names(dat)[1]), 
-               y = as.name(names(dat)[2]),
-               colour=as.name(names(dat)[ncol(dat)])))+ geom_point(size=4, alpha = 0.5)+
-               scale_color_manual(values=c("blue","red"))+ theme(legend.position = 'none'),vp=vplayout(i,j))
+  for(j in 1:(i-1)){ 
+  
+  g<-ggplot(dat, 
+         aes_q(x = as.name(names(dat)[j]), 
+               y = as.name(names(dat)[i]),
+               colour=as.name(names(dat)[ncol(dat)])))+ geom_point(size=1, alpha = 0.2)+
+               scale_color_manual(values=c("blue","red"))+ theme(legend.position = 'none')+
+               theme(panel.grid.minor=element_blank(),panel.grid.major=element_blank(),
+               plot.margin=unit(c(0,0,0,0),"mm"))+theme(axis.text.y = element_text(angle = 90, hjust = 1))
+  if(j!=1) g<-g+ scale_y_discrete(breaks=NULL)+ylab("")
+  if(i!=(ncol(dat)-1)) g<-g+ scale_x_discrete(breaks=NULL)+xlab("")
+  print(g,vp=vplayout(i,j))             
   }  
 }
 #color above the diagonal
+  Cols<-c("white",brewer.pal(9,"YlOrRd"))
+  
   if(i<(ncol(dat)-1)){
-    for(j in 1:(i-1)){
+    for(j in (i+1):(ncol(dat)-1)){
+      Cor<-cor(dat[,i],dat[,j])
+      ColIndx<-cut(abs(Cor),breaks=c(0,seq(from=.5,to=1,by=.1)))
       print(
-        ggplot(dat, 
-               aes_q(x = as.name(names(dat)[1]), 
-                     y = as.name(names(dat)[2]),
-                     colour=as.name(names(dat)[ncol(dat)])))+ geom_blank(size=4, alpha = 0.5)+
-                     theme(panel.background = element_rect(fill = "red"))+
-          scale_color_manual(values=c("blue","red"))+ theme(legend.position = 'none'),vp=vplayout(i,j))
+        ggplot(d,  aes(x = x, y = y))+ geom_blank(size=4, alpha = 0.5)+
+                     theme(panel.background = element_rect(fill = Cols[ColIndx]))+
+                     theme(panel.grid.major = element_line(colour = Cols[ColIndx]))+
+                     theme(panel.grid.minor = element_line(colour = Cols[ColIndx]),
+                     plot.margin=unit(c(0,0,0,0),"mm"))+
+                     ylab("")+xlab("")+scale_x_discrete(breaks=NULL)+
+                     scale_y_discrete(breaks=NULL)+
+                     annotate("text", label= round(Cor,digits=2), x=1.2, y=1.2,
+                              size=15*abs(Cor)),
+          vp=vplayout(i,j))
     }  
   }
     
   }
 #========================================
 #========================================
-changeMonth <- base::as.Date("1996-02-15") #as.Date("1995-04-19") + lubridate::weeks(39) = "1996-01-17"
-
-vpLayout <- function(x, y) { grid::viewport(layout.pos.row=x, layout.pos.col=y) }
-
-fullSpread <- function( scores ) { 
-  return( base::range(scores) ) #A new function isn't necessary.  It's defined in order to be consistent.
-}
-hSpread <- function( scores ) { 
-  return( stats::quantile(x=scores, probs=c(.25, .75)) ) 
-}
-seSpread <- function( scores ) { 
-  return( base::mean(scores) + base::c(-1, 1) * stats::sd(scores) / base::sqrt(base::sum(!base::is.na(scores))) ) 
-}
-bootSpread <- function( scores, conf=.68 ) {
-  plugin <- function( d, i ) { base::mean(d[i]) }
-  
-  distribution <- boot::boot(data=scores, plugin, R=99) #999 for the publication
-  ci <- boot::boot.ci(distribution, type = c("bca"), conf=conf)
-  return( ci$bca[4:5] ) #The fourth & fifth elements correspond to the lower & upper bound.
-}
-
-darkTheme <- ggplot2::theme(
-  axis.title          = ggplot2::element_text(color="gray30", size=9),
-  axis.text.x         = ggplot2::element_text(color="gray30", hjust=0),
-  axis.text.y         = ggplot2::element_text(color="gray30"),
-  axis.ticks.length   = grid::unit(0, "cm"),
-  axis.ticks.margin   = grid::unit(.00001, "cm"),
-  #   panel.grid.minor.y  = element_line(color="gray95", size=.1),
-  #   panel.grid.major    = element_line(color="gray90", size=.1),
-  panel.margin        = grid::unit(c(0, 0, 0, 0), "cm"),
-  plot.margin         = grid::unit(c(0, 0, 0, 0), "cm")
-)
-
-p <- ggplot(dat, 
-              aes_q(x = as.name(names(dat)[1]), 
-                    y = as.name(names(dat)[2]),
-                    colour=as.name(names(dat)[ncol(dat)])))+ geom_point(size=4, alpha = 0.5)+
-                   scale_color_manual(values=c("blue","red"))
-
-print(qplot(dat[,i],
-      geom="blank",
-      main = names(dat[,i]), 
-      xlab = "Age",  
-      fill=I("blue")),vp=vplayout(1,2))
-
-print(TmaxCompare+ ggtitle(ParkName)+theme(plot.title =element_text(size=rel(2.5))), vp=vplayout(1,1))
-print(TminCompare, vp=vplayout(2,1))
-print(PptCompare, vp=vplayout(3,1))
-dev.off()   
-
-print(qplot(dat[,1],
-      geom="histogram",
-      binwidth = 0.5,  
-      main = names(dat[,1]), 
-      xlab = "Age",  
-      fill=I("blue")),vp=vplayout(1,1))
