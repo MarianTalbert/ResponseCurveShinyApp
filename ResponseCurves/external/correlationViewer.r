594a59756d0bef7ec806d85e4ede8074080b6f72
Talbert <mtalbert@usgs.gov>
2016-03-31 11:37:13 -0600

    it worked to put the brush correlation into the bigger pairsplot app but not everything is hooked up

--- ResponseCurves/external/correlationViewer.r
+++ ResponseCurves/external/correlationViewer.r
@@ -29,10 +29,12 @@ app <- shinyApp(
               checkboxInput("showRespGam","Show GAM fit each predictor with the Response",value=FALSE),
               numericInput("numPlts","Number of variables to display" , 5),
               sliderInput("pointSize","Scatterplot point size",min=.05,max=6,value=1),
-              sliderInput("alpha","Scatterplot point transparency",min=.05,max=1,value=.7)
+              sliderInput("alpha","Scatterplot point transparency",min=.05,max=1,value=.7),
+              actionButton("highlight", label = "Highlight points in brushed area"),
+              actionButton("resethighlight", label = "remove highlight selection")
           ),
           mainPanel(
-            plotOutput("parisPlot",height=1000,width=1000)
+            uiOutput('plots')
           )
         )
       ),
@@ -60,6 +62,84 @@ app <- shinyApp(
      ))  
     ),
     server = function(input, output) {
+      XYs <- reactiveValues(
+        brushRegion = rep(FALSE,times=nrow(dat))
+      )
+      for (i in 1:Nplots) {
+        local({
+          num <- i # Make local variable
+          plotname <- paste("plot", num , sep="")
+          output[[plotname]] <- renderPlot({
+            colNum<-num %% (n.col+ShowResp)
+            rowNum<-ceiling(num/(n.col+ShowResp))
+            #modular arithmitic doesn't quite map how I need
+            if(colNum==0) colNum<-n.col+ShowResp
+            #use the zero column for the relationship bw resp and pred if it is to be used
+            if(ShowResp) colNum<-colNum-1 
+            
+            ggpairs(dat,alph=.5,pointSize=1,DevScore=2,showResp=ShowResp,brushRegion=XYs$brushRegion,
+                    rowNum=rowNum,colNum=colNum)
+            
+          })
+        })
+      }
+      
+      observeEvent(input$resethighlight,{
+        XYs$brushRegion = rep(FALSE,times=nrow(dat))
+      })
+      observeEvent(input$highlight,{
+        
+        Name<-names(input)[grep("plotbrush",names(input))]
+        
+        #shiny gets angry if I check for null in an lapply so do a loop over names
+        #to figure out which plotbrush is non-null
+        for(i in 1:length(Name)) if(!is.null(input[[Name[i]]])) indx=i
+        Name<-Name[indx]
+        brush<-input[[Name]]
+        plotNum<-gsub("plotbrush","",Name)
+        
+        #I need to make sure this works 
+        Yvar<-ceiling(as.numeric(plotNum)/(n.col+ShowResp))
+        Xvar<-as.numeric(plotNum)%%(n.col+ShowResp)-ShowResp
+        
+        if (!is.null(brush)) {
+          #three cases for the response look at the X and the correct response
+          #for the hist the Yvar is wrong because it's a count
+          
+          #X and y are actually different for the first column
+          if(Xvar==0) XYs$brushRegion<-dat[,Yvar]<=brush$xmax & dat[,Yvar]>=brush$xmin &
+              dat[,ncol(dat)]<=brush$ymax & dat[,ncol(dat)]>=brush$ymin
+          if(Xvar==Yvar) XYs$brushRegion<-dat[,Xvar]<=brush$xmax & dat[,Xvar]>=brush$xmin
+          if(Xvar!=0 & Xvar!=Yvar) XYs$brushRegion<-dat[,Xvar]<=brush$xmax & dat[,Xvar]>=brush$xmin &
+              dat[,Yvar]<=brush$ymax & dat[,Yvar]>=brush$ymin
+          
+        } else {
+          XYs$brushRegion <-rep(FALSE,times=nrow(dat))
+        }
+      })
+      
+      output$plots <- renderUI({
+        col.width <- round(12/(n.col+ShowResp)) # Calculate bootstrap column width
+        n.row <- ceiling(Nplots/(n.col+ShowResp)) # calculate number of rows
+        cnter <<- 0 # Counter variable
+        
+        # Create row with columns
+        rows  <- lapply(1:n.row,function(row.num){
+          cols  <- lapply(1:(n.col+ShowResp), function(i) {
+            cnter    <<- cnter + 1
+            brushName<-paste("plotbrush",cnter,sep="")
+            plotname <- paste("plot", cnter, sep="")
+            column(col.width, plotOutput(plotname,height="300px",
+                                         brush = brushOpts(
+                                           id = brushName,
+                                           resetOnNew = TRUE
+                                         )),style="padding: 1px;")
+          }) 
+          fluidRow( do.call(tagList, cols),style="padding: 1px;" )
+        })
+        
+        do.call(tagList, rows)
+      })
       values<-reactiveValues(orderVar=TotalCors,
                              dat=dat,
                              devExp=DevScore$devExp,
@@ -87,23 +167,7 @@ app <- shinyApp(
        radioButtons('InptVar', 'Variable', choices=choices)
       })
   
-      output$parisPlot <- renderPlot({
-          #split off the percent deviance explained
-        if(!is.null(input$chkGrp)){
-            spltDat<-strsplit(input$chkGrp," ")
-            varsToUse<-unlist(lapply(spltDat,"[",1))
-            d<-values$dat[,c(match(varsToUse,names(values$dat)))]
-            
-            if(input$numPlts<(ncol(d))) d<-d[,(1:input$numPlts)]
-            d<-cbind(d,resp=values$dat[,ncol(values$dat)])
-        
-          ggpairs(dat=d,alph=input$alpha,pointSize=input$pointSize,DevScore=DevScore,
-                  showResp=input$showRespGam)
-       }
-       })
-      
-      
-      
+     
       output$VarMap<-renderPlot({
         if(!is.null(input$InptVar)){
           InputVar<-strsplit(input$InptVar," ")[[1]][1]
